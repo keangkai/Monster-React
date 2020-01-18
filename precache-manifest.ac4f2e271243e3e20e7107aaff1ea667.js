6753d92bd463261e55e69236686b9d4d0a684ba8
Keangkai Poungmanee <m.keangkai@gmail.com>
2020-01-18 16:20:47 +0700

    Updates

---
+++ precache-manifest.ac4f2e271243e3e20e7107aaff1ea667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74125b50120a8bea4346803c90db0110",
+    "url": "/Monster-React/index.html"
+  },
+  {
+    "revision": "5e5d86f64fef998b7785",
+    "url": "/Monster-React/static/css/main.2d4f447b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/Monster-React/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster-React/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e5d86f64fef998b7785",
+    "url": "/Monster-React/static/js/main.5b17e44d.chunk.js"
+  },
+  {
+    "revision": "32680c066d1a51dbbc6a",
+    "url": "/Monster-React/static/js/runtime-main.76b8790c.js"
+  }
+]);
